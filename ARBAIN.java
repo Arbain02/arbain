5cc317bb18ba09842e9148cfa3fa1ee07b395564
Arbain02 <147217625+Arbain02@users.noreply.github.com>
2023-10-09 23:23:07 -0700

    Add files via upload

---
+++ ARBAIN.java
@@ -0,0 +1,13 @@
+
+package tgs2_isc;
+
+public class ARBAIN {
+    public static void main(String[] args) {
+    String nama = "ARBAIN";
+    int umur = 21;
+    
+    
+    System.out.println("nama saya " + nama + ".");
+    System.out.println("Saya berusia " + umur + " tahun.");
+    } 
+}
